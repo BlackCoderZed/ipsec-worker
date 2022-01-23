051a5633418b03226449be58f9b1d1b3abc0ec97
BlackCoderZed <41987824+BlackCoderZed@users.noreply.github.com>
2022-01-23 21:10:10 +0630

    Update ipsec-register.py

--- ipsec-register.py
+++ ipsec-register.py
@@ -1,1 +1,20 @@
+import pyodbc
+import os
+import email, smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
+#########################################################################################
+#                                  KeyInfo Class                                        #
+#########################################################################################
+
+class KeyInfo:
+    def __init__(self, TicketId, KeyName, Password, Email):
+        self.TicketId = TicketId
+        self.KeyName = KeyName
+        self.Email = Email
+        self.Password = Password
+
+def Generate(keyInfo):

b8a96f68492a92953256421dc2bcdf8c7e72d27e
BlackCoderZed <blackcoder.zed@gmail.com>
2022-01-24 22:13:06 +0630

    [Add] Email Send

--- ipsec-register.py
+++ ipsec-register.py
@@ -17,4 +17,91 @@ class KeyInfo:
         self.Email = Email
         self.Password = Password
 
-def Generate(keyInfo):
+def GetTicketInfo(serverId):
+    ticketInfoLst = []
+    server = 'tcp:13.231.65.63' 
+    database = 'It-Solution-OpenVPN' 
+    username = 'sa' 
+    password = 'Superm@n' 
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+
+    query = """select Inst.TicketID,Inst.CommandCode,KeyInfo.Name as [KeyName],Cust.EmailAddress as [Emai] from Instructions as Inst
+        inner join KeyInformations as KeyInfo on Inst.KeyInfoId = KeyInfo.Id inner join Customers as Cust on KeyInfo.CustomerID = Cust.CustomerId 
+        where Inst.CommandCode = 101 and Inst.InstructionStatusID = 1 and Inst.ServerID = """+serverId+""";"""
+
+
+    cursor.execute(query)
+    resultLst = cursor.fetchall()
+
+    for result in resultLst:
+        ticketId = result[0]
+        keyName = result[2]
+        email = result[3]
+        kInfo = KeyInfo(ticketId, keyName, '', email)
+        ticketInfoLst.append(kInfo)
+    cursor.close()
+    cnxn.close()
+
+    return ticketInfoLst
+
+# Instruction Status Update
+def UpdateTicketInfo(ticketInfo):
+    server = 'tcp:13.231.65.63' 
+    database = 'It-Solution-OpenVPN' 
+    username = 'sa' 
+    password = 'Superm@n' 
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+
+    query = "update Instructions Set InstructionStatusID = 2 where TicketID = "+ str(ticketInfo.TicketId) +";"
+    cursor.execute(query)
+    cursor.commit()
+    cursor.close()
+    cnxn.close()
+    print('Updated')
+
+# Send Mail to receiver
+def SendMail(ticketInfo):
+    print('...Sending...')
+    subject = "OpenVPN by IT-Solution"
+    body = "Thanks for choosing IT-Solution.\n***Automated email***"
+    sender_email = "blackcoder.zed@gmail.com"
+    receiver_email = ticketInfo.Email
+    filename = HOME_DIR + ticketInfo.KeyName + '.txt'
+    password = 'Password'
+    attachName = ticketInfo.KeyName + '.ovpn'
+
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email
+
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
+
+    with open(filename, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    encoders.encode_base64(part)
+
+    # Add header as key/value pair to attachment part
+    part.add_header(
+                "Content-Disposition",
+                f"attachment; filename= {attachName}",
+    )
+
+    # Add attachment to message and convert message to string
+    message.attach(part)
+    text = message.as_string()
+
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
+    print('Send...')
+

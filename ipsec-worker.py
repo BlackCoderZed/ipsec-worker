300d58517efde5fbffa349ae76b053081cfc7b4f
Blackcoder Zed <41987824+BlackCoderZed@users.noreply.github.com>
2024-06-12 19:49:27 +0630

    Update ipsec-worker.py

--- ipsec-worker.py
+++ ipsec-worker.py
@@ -153,7 +153,7 @@ def SendMail(ticketInfo):
 
     # Log in to server using secure context and send email
     context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(SMTP_ADDRESS, 587) as server:
+    with smtplib.SMTP(SMTP_ADDRESS, 587) as server:
         server.starttls(context=context)
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, text)
